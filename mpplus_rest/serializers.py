ddb7ea56cd90da26493c4c31b6912e87c31a4d90
ebarbara <estevan.barbara@gmail.com>
2018-10-05 19:52:22 -0300

    Complemento do endpoint de Temas

--- mpplus_rest/serializers.py
+++ mpplus_rest/serializers.py
@@ -21,7 +21,6 @@ class AreaSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Area
-        depth = 1
         fields = (
             'id',
             'nome',
@@ -35,7 +34,56 @@ class AreaSerializer(serializers.ModelSerializer):
         return obj.temas.count() + obj.temas_correlatos.count()
 
 
+class InternalAreaSerializer(serializers.ModelSerializer):
+    icone = IconeSerializer()
+
+    class Meta:
+        model = Area
+        fields = (
+            'id',
+            'nome',
+            'cor',
+            'icone',
+        )
+
+
 class TemaSerializer(serializers.ModelSerializer):
+    data_criacao = serializers.DateTimeField(
+        source='created_at',
+        format='%d/%m/%Y'
+    )
+    endpoint = serializers.SerializerMethodField()
+    area_mae = InternalAreaSerializer()
+    areas_correlatas = serializers.PrimaryKeyRelatedField(
+        many=True,
+        read_only=True
+    )
+    fonte = serializers.CharField(
+        source='fonte_dados',
+        allow_blank=True
+    )
+    url = serializers.URLField(
+        source='url_tableau',
+        allow_blank=True
+    )
+
     class Meta:
         model = Tema
-        fields = ('id', 'titulo')
+        depth = 1
+        fields = (
+            'id',
+            'data_criacao',
+            'titulo',
+            'endpoint',
+            'area_mae',
+            'areas_correlatas',
+            'subtitulo',
+            'descricao',
+            'fonte',
+            'observacao',
+            'url',
+            'prioridade',
+        )
+
+    def get_endpoint(self, obj):
+        return obj.titulo.replace(' ', '')

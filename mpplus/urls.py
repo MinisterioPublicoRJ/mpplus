666e31b869b3a49b635544e3f0ca22732fc7681c
ebarbara <estevan.barbara@gmail.com>
2018-10-29 18:03:00 -0300

    Sistema de login via SCA

--- mpplus/urls.py
+++ mpplus/urls.py
@@ -18,8 +18,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from usercontrol.views import login
+
 
 urlpatterns = [
     path('api/', include('mpplus_rest.urls')),
+    path('login/', login, name='login'),
+    path('admin/login/', login, name='login'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
